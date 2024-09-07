a78296cf546a1bce9781b790fbd00747b3048a97
Ashwini Kumar <shahi.ashwini29@gmail.com>
2024-09-07 11:32:22 +0530

    get request added

--- controllers/student.js
+++ controllers/student.js
@@ -1,5 +1,29 @@
 const db = require("../dbConfig");
 
+const getAllStudents = async (req, res) => {
+  try {
+    const data = await db.query(`SELECT * FROM student`);
+    if (!data) {
+      return res.status(404).json({
+        success: false,
+        message: "No students found",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      message: "student data fetched successfully",
+      totalStudents: data[0].length,
+      students: data[0],
+    });
+  } catch (error) {
+    console.log(`error retrieving data form database ${error}`);
+    res.status(500).send({
+      success: false,
+      message: error,
+    });
+  }
+};
+
 const handleAddStudents = async (req, res) => {
   try {
     const { roll_no, name, classCurrent, fees, age, address } = req.body;
@@ -36,4 +60,4 @@ const handleAddStudents = async (req, res) => {
   }
 };
 
-module.exports = { handleAddStudents };
+module.exports = { getAllStudents, handleAddStudents };
--- routes/student.js
+++ routes/student.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { handleAddStudents } = require("../controllers/student");
+const { handleAddStudents, getAllStudents } = require("../controllers/student");
+
+router.get("/getAllStudents", getAllStudents);
 
 router.post("/addStudent", handleAddStudents);
 
