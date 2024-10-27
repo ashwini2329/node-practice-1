906583e71a092a107401aac5ba02e3a333060aa7
Ashwini Kumar <shahi.ashwini29@gmail.com>
2024-10-27 14:28:25 +0530

    GET & POST request handled through sequelize

--- controllers/student.js
+++ controllers/student.js
@@ -1,65 +1,49 @@
 const db = require("../dbConfig");
+const Student = require("../models/student");
 
 // GET Request - Get all data from the student table
 const getAllStudents = async (req, res) => {
-  try {
-    const data = await db.query(`SELECT * FROM student`);
-    if (!data) {
-      return res.status(404).json({
-        success: false,
-        message: "No students found",
+  Student.findAll()
+    .then((students) => {
+      res.status(201).json({
+        success: true,
+        data: students,
       });
-    }
-    res.status(200).send({
-      success: true,
-      message: "student data fetched successfully",
-      totalStudents: data[0].length,
-      students: data[0],
+    })
+    .catch((error) => {
+      res.status(500).json({
+        success: false,
+        message: "Error fetching students",
+        error: error.message,
+      });
     });
-  } catch (error) {
-    console.log(`error retrieving data form database ${error}`);
-    res.status(500).send({
-      success: false,
-      message: error,
-    });
-  }
 };
 
 // POST Request - Adding student into student table
 const handleAddStudents = async (req, res) => {
-  try {
-    const { roll_no, name, classCurrent, fees, age, address } = req.body;
-    if (!roll_no || !name || !classCurrent || !fees || !age || !address) {
-      console.log(`All data are required to add a student`);
-      res.status(400).send({
-        success: false,
-        message: "All fields are required",
+  const { roll_no, name, classCurrent, fees, age, address } = req.body;
+  Student.create({
+    roll_no,
+    name,
+    classCurrent,
+    fees,
+    age,
+    address,
+  })
+    .then((student) => {
+      res.status(201).json({
+        success: true,
+        data: student,
+        message: "Student created successfully",
       });
-    }
-
-    const data = await db.query(
-      `INSERT INTO student (roll_no, name, classCurrent, fees, age, address) VALUES(?, ?, ?, ?, ? ,?)`,
-      [roll_no, name, classCurrent, fees, age, address]
-    );
-    if (!data) {
-      console.log(`Failed to add student`);
-      res.status(404).send({
+    })
+    .catch((error) => {
+      res.status(500).json({
         success: false,
-        message: "Failed to add student",
+        message: "Error creating student",
+        error: error.message,
       });
-    }
-    res.status(201).send({
-      success: true,
-      message: "Student added successfully",
     });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({
-      success: false,
-      message: "Error adding student",
-      error: error,
-    });
-  }
 };
 
 // DELETE Request - Deleting student by Id from student table
