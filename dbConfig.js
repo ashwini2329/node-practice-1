826372b649b8e641d3454d81ab25b417cda0b8a7
Ashwini Kumar <shahi.ashwini29@gmail.com>
2024-11-04 23:41:08 +0530

    .env package installed & configured

--- dbConfig.js
+++ dbConfig.js
@@ -1,5 +1,10 @@
-const Sequelize = require('sequelize')
+const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize('node_practice_1', 'root', 'mySQL2329', {dialect: 'mysql', host: 'localhost'});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  { dialect: "mysql", host: process.env.DB_HOST }
+);
 
 module.exports = sequelize;
