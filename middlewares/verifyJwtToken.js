bfcf2fdf7551e759f1466f30936ae95e31c98c38
Ashwini Kumar <shahi.ashwini29@gmail.com>
2024-11-15 22:21:53 +0530

    token verification middleware added

---
+++ middlewares/verifyJwtToken.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const secretKey = process.env.JWT_SECRET;
+
+const verifyToken = (req, res, next) => {
+  const token =
+    req.header("Authorization") && req.header("Authorization").split(" ")[1];
+  console.log(`token = ${token}`);
+  if (!token) {
+    return res.status(401).json({ message: "No token, authorization denied" });
+  }
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    console.log(`decoded = ${JSON.stringify(decoded)}`);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Token is not valid" });
+  }
+};
+
+module.exports = verifyToken;
