d77d9fb6303d7a87e37bc5e55085f6edb6988816
Ashwini Kumar <shahi.ashwini29@gmail.com>
2024-11-15 22:21:02 +0530

    index.js formatted

--- index.js
+++ index.js
@@ -2,23 +2,17 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-
-app.use(cors());
 const sequelize = require("./dbConfig");
+const PORT = process.env.PORT || 8080;
+app.use(cors());
+app.use(express.json());
 
-// Import your model
 const Student = require("./models/student");
 const UserSignUp = require("./models/user");
-
-const PORT = 8080;
 const studentRoute = require("./routes/student");
 const employeesRoute = require("./routes/employee");
 const userRoute = require("../node-practice-1/routes/user");
 
-// Middlewares
-app.use(express.json());
-
-// Routes
 app.use("/users", userRoute);
 app.use("/students", studentRoute);
 // app.use("/employees", employeesRoute);
@@ -27,9 +21,8 @@ app.get("/", (req, res) => {
   res.status(200).send("<h1>Hello from Node.Js</h1>");
 });
 
-// Sync the database schema and start the server
 sequelize
-  .sync() // You can also use .sync({ alter: true }) to update tables if the model structure changes
+  .sync()
   .then(() => {
     console.log("Database synced successfully.");
     return sequelize.query("SELECT 1"); // Optional check to verify the connection
